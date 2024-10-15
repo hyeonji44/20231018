0bb0484b25630ff32358788bb9ea51a5b4121485
Ju, Hyeonji <hyeonji443@gmail.com>
2024-10-15 21:15:44 +0900

    two

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 202113018");
 	}
 }
---
+++ src/Two.java
@@ -0,0 +1,5 @@
+public class Two {
+    public static void print() {
+    System.out.println("two");
+    }
+}
